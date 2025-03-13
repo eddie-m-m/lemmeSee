509730ee736c7a78117f68815498cce5e2850772
eddie-m-m <eddie.m.menefee@gmail.com>
2025-03-13 09:15:25 -0700

    Add basic router

--- main.go
+++ main.go
@@ -5,20 +5,31 @@ import (
 	"net/http"
 )
 
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		http.NotFound(w, r)
+		// TODO: handle page not found error
+	}
+}
+
 func contactHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, `<h1>Contact Page</h1><p>Wanna connect? email me at
 	 <a href="mailto:eddie.m.menefee@gmail.com">eddie.m.menefee@gmail.com</p>`)
 }
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Test 2!</h1>")
 }
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.HandleFunc("/contact", contactHandler)
+	http.HandleFunc("/", pathHandler)
 	fmt.Println("Starting the server on :3000...")
 	http.ListenAndServe(":3000", nil)
 }
