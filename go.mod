6789822792e1f1d88a11c955a0428dc75e5564b7
eddie-m-m <eddie.m.menefee@gmail.com>
2025-03-13 10:19:50 -0700

    Add chi routing

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/eddie-m-m/lemmeSee
 
 go 1.24.1
+
+require github.com/go-chi/chi/v5 v5.2.1 // indirect
--- main.go
+++ main.go
@@ -3,7 +3,13 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
+
+func setContentTypeHeader(w http.ResponseWriter) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+}
 
 func pathHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.URL.Path {
@@ -12,24 +18,36 @@ func pathHandler(w http.ResponseWriter, r *http.Request) {
 	case "/contact":
 		contactHandler(w, r)
 	default:
-		http.NotFound(w, r)
-		// TODO: handle page not found error
+		// http.NotFound(w, r)
+		http.Error(w, "Page not found", http.StatusNotFound)
 	}
 }
 
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	setContentTypeHeader(w)
+	fmt.Fprint(w, `<h1>FAQ</h1>`)
+}
+
 func contactHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	setContentTypeHeader(w)
 	fmt.Fprint(w, `<h1>Contact Page</h1><p>Wanna connect? email me at
 	 <a href="mailto:eddie.m.menefee@gmail.com">eddie.m.menefee@gmail.com</p>`)
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	setContentTypeHeader(w)
 	fmt.Fprint(w, "<h1>Test 2!</h1>")
 }
 
 func main() {
-	http.HandleFunc("/", pathHandler)
+	// http.HandleFunc("/", pathHandler)
+	r := chi.NewRouter()
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
+	r.Get("/faq", faqHandler)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Page not found", http.StatusNotFound)
+	})
 	fmt.Println("Starting the server on :3000...")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", r)
 }
