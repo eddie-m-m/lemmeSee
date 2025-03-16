81a1a4e0cd7dae9b1c702bf887a9b00de4b9f6c1
eddie-m-m <eddie.m.menefee@gmail.com>
2025-03-15 17:52:48 -0700

    Add templates, controllers dirs

--- cmd/exp/exp.go
+++ cmd/exp/exp.go
@@ -1,33 +1,8 @@
 package main
 
-import (
-	"html/template"
-	"os"
-)
-
-type Hobby struct {
-	Name template.HTML
-}
-type User struct {
-	Name    template.HTML
-	Age     int
-	Hobbies []Hobby
-}
+import "fmt"
 
 func main() {
-	t, err := template.ParseFiles("hello.gohtml")
-	if err != nil {
-		panic(err)
-	}
-
-	user := User{
-		Name:    "Dude",
-		Age:     50,
-		Hobbies: []Hobby{{Name: "Reading"}, {Name: "Coding"}},
-	}
-
-	err = t.Execute(os.Stdout, user)
-	if err != nil {
-		panic(err)
-	}
+	numbers := []int{1, 2, 3}
+	fmt.Println(numbers[4])
 }
--- main.go
+++ main.go
@@ -2,66 +2,39 @@ package main
 
 import (
 	"fmt"
-	"html/template"
-	"log"
 	"net/http"
 	"path/filepath"
 
+	"github.com/eddie-m-m/lemmeSee/controllers"
+	"github.com/eddie-m-m/lemmeSee/views"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
-func setContentTypeHeader(w http.ResponseWriter) {
+func SetContentTypeHeader(w http.ResponseWriter) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-}
-
-func galleryHandler(w http.ResponseWriter, r *http.Request) {
-	galleryID := chi.URLParam(r, "galleryID")
-	setContentTypeHeader(w)
-	fmt.Fprint(w, "<h1>Gallery ID: "+galleryID+"</h1>")
-}
-
-func faqHandler(w http.ResponseWriter, r *http.Request) {
-	setContentTypeHeader(w)
-	executeTemplate(w, "faq.gohtml")
-}
-
-func contactHandler(w http.ResponseWriter, r *http.Request) {
-	setContentTypeHeader(w)
-	executeTemplate(w, "contact.gohtml")
-}
-
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	setContentTypeHeader(w)
-	executeTemplate(w, "home.gohtml")
-}
-
-func executeTemplate(w http.ResponseWriter, filePath string) {
-	tplPath := filepath.Join("templates", filePath)
-	tpl, err := template.ParseFiles(tplPath)
-	if err != nil {
-		log.Printf("parsing template: %v", err)
-		http.Error(w, "There was an error parsing the template.", http.StatusInternalServerError)
-		return
-	}
-	err = tpl.Execute(w, nil)
-	if err != nil {
-		log.Printf("executing template: %v", err)
-		http.Error(w, "There was an error executing the template.", http.StatusInternalServerError)
-		return
-	}
 }
 
 func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-	r.Get("/", homeHandler)
-	r.Get("/contact", contactHandler)
-	r.Get("/faq", faqHandler)
-	r.Get("/galleries/{galleryID}", galleryHandler)
+
+	tpl := views.Must(views.Parse(filepath.Join("templates", "home.gohtml")))
+	r.Get("/", controllers.StaticHandler(tpl))
+
+	tpl = views.Must(views.Parse(filepath.Join("templates", "contact.gohtml")))
+	r.Get("/contact", controllers.StaticHandler(tpl))
+
+	tpl = views.Must(views.Parse(filepath.Join("templates", "faq.gohtml")))
+	r.Get("/faq", controllers.StaticHandler(tpl))
+
+	tpl = views.Must(views.Parse(filepath.Join("templates", "news.gohtml")))
+	r.Get("/news", controllers.StaticHandler(tpl))
+
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
 	})
+
 	fmt.Println("Starting the server on :3000...")
 	http.ListenAndServe(":3000", r)
 }
