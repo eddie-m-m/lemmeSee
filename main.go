ae259aef36072cfb4545caafdf08b44a05a9dd6d
eddie-m-m <eddie.m.menefee@gmail.com>
2025-03-15 19:03:58 -0700

    Add template embbedding

--- main.go
+++ main.go
@@ -3,33 +3,25 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"path/filepath"
 
 	"github.com/eddie-m-m/lemmeSee/controllers"
+	"github.com/eddie-m-m/lemmeSee/templates"
 	"github.com/eddie-m-m/lemmeSee/views"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
-func SetContentTypeHeader(w http.ResponseWriter) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-}
-
 func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 
-	tpl := views.Must(views.Parse(filepath.Join("templates", "home.gohtml")))
-	r.Get("/", controllers.StaticHandler(tpl))
+	r.Get("/", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "home.gohtml"))))
 
-	tpl = views.Must(views.Parse(filepath.Join("templates", "contact.gohtml")))
-	r.Get("/contact", controllers.StaticHandler(tpl))
+	r.Get("/contact", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "contact.gohtml"))))
 
-	tpl = views.Must(views.Parse(filepath.Join("templates", "faq.gohtml")))
-	r.Get("/faq", controllers.StaticHandler(tpl))
+	r.Get("/faq", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "faq.gohtml"))))
 
-	tpl = views.Must(views.Parse(filepath.Join("templates", "news.gohtml")))
-	r.Get("/news", controllers.StaticHandler(tpl))
+	r.Get("/news", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "news.gohtml"))))
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
